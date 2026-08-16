f30676244001163f3100e4a0e5f2642eb4e21cc4
Brent <zburns1980@gmail.com>
2026-08-16 14:45:00 -0600

    fixes name conflict bug

--- bash/.config/bash/fns/tool-help.sh
+++ bash/.config/bash/fns/tool-help.sh
@@ -1,9 +1,13 @@
 # shows --help piped into bat for the tool that is passed as an args
-# for example: h ls will show ls --help piped to bat
-h() {
-  [[ $# -eq 0 ]] && { echo "usage: h <command> [args...]" >&2; return 1; }
-  command -v bat >/dev/null || { "$@" --help 2>&1 | ${PAGER:-less}; return; }
+# for example: hlp ls will show ls --help piped to bat
+hlp() {
+  [[ $# -eq 0 ]] && {
+    echo "usage: hlp <command> [args...]" >&2
+    return 1
+  }
+  command -v bat >/dev/null || {
+    "$@" --help 2>&1 | ${PAGER:-less}
+    return
+  }
   "$@" --help 2>&1 | bat --plain --language=help
 }
-
-
